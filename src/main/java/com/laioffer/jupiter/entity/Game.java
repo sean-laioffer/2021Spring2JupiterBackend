4af8b39e74b6139ff49b8940c1f6169e4629a141
sean-laioffer <sean@laioffer.com>
2021-03-14 22:35:06 -0700

    Lesson 3 commit.

---
+++ src/main/java/com/laioffer/jupiter/entity/Game.java
@@ -0,0 +1,74 @@
+package com.laioffer.jupiter.entity;
+
+public class Game {
+    private String name;
+    private String developer;
+    private String releaseTime;
+    private String website;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public String getReleaseTime() {
+        return releaseTime;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Game(Builder builder) {
+        this.name = builder.name;
+        this.developer = builder.developer;
+        this.releaseTime = builder.releaseTime;
+        this.website = builder.website;
+        this.price = builder.price;
+    }
+
+    public static class Builder {
+        private String name;
+        private String developer;
+        private String releaseTime;
+        private String website;
+        private double price;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setDeveloper(String developer) {
+            this.developer = developer;
+            return this;
+        }
+
+        public Builder setReleaseTime(String releaseTime) {
+            this.releaseTime = releaseTime;
+            return this;
+        }
+
+        public Builder setWebsite(String website) {
+            this.website = website;
+            return this;
+        }
+
+        public Builder setPrice(double price) {
+            this.price = price;
+            return this;
+        }
+
+        public Game build() {
+            return new Game(this);
+        }
+    }
+}
