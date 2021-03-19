0fa438ffad6e2adb0d8d50be9663a8bebb535787
sean-laioffer <sean@laioffer.com>
2021-03-18 22:08:38 -0700

    Lesson 7 Commit.

--- src/main/java/com/laioffer/jupiter/entity/Game.java
+++ src/main/java/com/laioffer/jupiter/entity/Game.java
@@ -1,80 +1,65 @@
 package com.laioffer.jupiter.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonDeserialize(builder = Game.Builder.class)
 public class Game {
+    @JsonProperty("id")
+    private final String id;
+
     @JsonProperty("name")
-    private String name;
+    private final String name;
 
-    @JsonProperty("developer")
-    private String developer;
+    @JsonProperty("box_art_url")
+    private final String boxArtUrl;
 
-    @JsonProperty("release_time")
-    private String releaseTime;
+    private Game(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.boxArtUrl = builder.boxArtUrl;
+    }
 
-    @JsonProperty("website")
-    private String website;
-
-    @JsonProperty("price")
-    private double price;
+    public String getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
     }
 
-    public String getDeveloper() {
-        return developer;
+    public String getBoxArtUrl() {
+        return boxArtUrl;
     }
 
-    public String getReleaseTime() {
-        return releaseTime;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public Game(Builder builder) {
-        this.name = builder.name;
-        this.developer = builder.developer;
-        this.releaseTime = builder.releaseTime;
-        this.website = builder.website;
-        this.price = builder.price;
-    }
-
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class Builder {
-        private String name;
-        private String developer;
-        private String releaseTime;
-        private String website;
-        private double price;
+        @JsonProperty("id")
+        private String id;
 
-        public Builder setName(String name) {
+        @JsonProperty("name")
+        private String name;
+
+        @JsonProperty("box_art_url")
+        private String boxArtUrl;
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
             this.name = name;
             return this;
         }
 
-        public Builder setDeveloper(String developer) {
-            this.developer = developer;
-            return this;
-        }
-
-        public Builder setReleaseTime(String releaseTime) {
-            this.releaseTime = releaseTime;
-            return this;
-        }
-
-        public Builder setWebsite(String website) {
-            this.website = website;
-            return this;
-        }
-
-        public Builder setPrice(double price) {
-            this.price = price;
+        public Builder boxArtUrl(String boxArtUrl) {
+            this.boxArtUrl = boxArtUrl;
             return this;
         }
 
--- src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
+++ src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
@@ -2,6 +2,8 @@ package com.laioffer.jupiter.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.laioffer.jupiter.entity.Game;
+import com.laioffer.jupiter.external.TwitchClient;
+import com.laioffer.jupiter.external.TwitchException;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
@@ -14,38 +16,23 @@ import java.io.IOException;
 
 @WebServlet(name = "GameServlet", urlPatterns = {"/game"})
 public class GameServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
-        String name = jsonRequest.getString("name");
-        String developer = jsonRequest.getString("developer");
-        String releaseTime = jsonRequest.getString("release_time");
-        String website = jsonRequest.getString("website");
-        float price = jsonRequest.getFloat("price");
-        // Print game information to IDE console
-        System.out.println("Name is: " + name);
-        System.out.println("Developer is: " + developer);
-        System.out.println("Release time is: " + releaseTime);
-        System.out.println("Website is: " + website);
-        System.out.println("Price is: " + price);
-        // Return status = ok as response body to the client
-        response.setContentType("application/json");
-        JSONObject jsonResponse = new JSONObject();
-        jsonResponse.put("status", "ok");
-        response.getWriter().print(jsonResponse);
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("application/json");
-        ObjectMapper mapper = new ObjectMapper();
+        // Get gameName from request URL.
+        String gameName = request.getParameter("game_name");
+        TwitchClient client = new TwitchClient();
 
-        Game.Builder builder = new Game.Builder();
-        builder.setName("World of Warcraft");
-        builder.setDeveloper("Blizzard Entertainment");
-        builder.setReleaseTime("Feb 11, 2005");
-        builder.setWebsite("https://www.worldofwarcraft.com");
-        builder.setPrice(49.99);
-
-        Game game = builder.build();
-        response.getWriter().print(mapper.writeValueAsString(game));
+        // Let the client know the returned data is in JSON format.
+        response.setContentType("application/json;charset=UTF-8");
+        try {
+            // Return the dedicated game information if gameName is provided in the request URL, otherwise return the top x games.
+            if (gameName != null) {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(client.searchGame(gameName)));
+            } else {
+                response.getWriter().print(new ObjectMapper().writeValueAsString(client.topGames(0)));
+            }
+        } catch (TwitchException e) {
+            throw new ServletException(e);
+        }
     }
 }
